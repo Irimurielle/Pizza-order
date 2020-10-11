d79b30d74eb5fc741c2608270f89bdf0ae19d1c4
Irimurielle <irimurielle@gmail.com>
2020-10-11 17:53:05 +0200

    function to get inputs

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#checkout").click(function () {
+        function size() {
+            var pizzaSize = document.getElementById("size").value;
+            return parseInt(pizzaSize);
+        }
+        function crust() {
+            var pizzaCrust = document.getElementById("crust").value;
+            return parseInt(pizzaCrust);
+        }
+        function toppings() {
+            var pizzaToppings = document.getElementById("toppings").value;
+            return parseInt(pizzaToppings);
+        }
+        function number() {
+            var pizzanumber = document.getElementById("number").value;
+            return parseInt(pizzaNumber);
+        }
+        
